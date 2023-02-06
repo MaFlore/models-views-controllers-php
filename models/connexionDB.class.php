8441208a76dab11b433c45c55f0f36ed4d9aeb92
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-02-06 23:30:51 +0000

    Ajout des commentaires au fichier connexionDB.class.php

--- models/connexionDB.class.php
+++ models/connexionDB.class.php
@@ -1,18 +1,21 @@
 <?php
-      class ConnexionDB{
+    //Création de la classe ConnexionDB qui se charge de se connecter à notre base de données
+    class ConnexionDB{
 
-            private $db_name = "revision";
-            private $password = "";
-            private $root = "root";
-            private $host = "localhost";
+        //Ajout des différents attributs
+        private $db_name = "revision";
+        private $password = "";
+        private $root = "root";
+        private $host = "localhost";
 
-            public function connexion (){
-                try {
-                    $bdd = new PDO('mysql:host='.$this -> host.';dbname='.$this->db_name.';charset=utf8', $this->root, $this->password);
-                    return $bdd;
-                } catch (Exception $e){
-                    echo 'Echec de connexion';
-                }
+        //Ajout de la fonction de connexion à la base de données
+        public function connexion (){
+            try {
+                $bdd = new PDO('mysql:host='.$this -> host.';dbname='.$this->db_name.';charset=utf8', $this->root, $this->password);
+                return $bdd;
+            } catch (Exception $e){
+                echo 'Connexion échouée';
             }
-      }
+        }
+    }
 ?>
