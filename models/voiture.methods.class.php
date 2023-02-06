f33cc4f7e30263bb1d900660574bf683eb77b832
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-02-06 23:48:06 +0000

    Ajout des commentaires au fichier voiture.methods.class.php

--- models/voiture.methods.class.php
+++ models/voiture.methods.class.php
@@ -1,17 +1,27 @@
 <?php
-  include_once('connexion.class.php');
+  //Inclusion du fichier de Connexion à la base de données
+  include_once('connexionDB.class.php');
+
+  //Inclusion du fichier comportant la classe voiture
   include('voiture.class.php');
 
+
+  //Création de la classe VoitureMethods héritant de la classe ConnexionDB et 
+  //comportant toutes les fonctions possibles d'un CRUD
   class VoitureMethods extends ConnexionDB{
     
+      //Fonction pour recupérer toutes les voitures
       public function get_all_voiture (){
+        //Appel de fonction connexion se trouvant dans le fichier de Connexion à la base de données
         $bd = $this->connexion();
         $requete = "SELECT * FROM voiture";
         $voitures= $bd->query($requete);
         return $voitures;
       }
 
+      //Fonction pour recupérer les détails d'une voiture à partir de l'ID
       public function get_voiture_by_id($id){
+        //Appel de fonction connexion se trouvant dans le fichier de Connexion à la base de données
         $bd = $this->connexion();
         $requete = "SELECT * FROM voiture WHERE id = :id";
         $voiture = $bd->prepare($requete);
@@ -22,7 +32,9 @@
         return $voiture;
       }
 
+      //Fonction d'ajout d'une voiture dans la base de données
       public function insert_voiture(Voiture $voiture){
+        //Appel de fonction connexion se trouvant dans le fichier de Connexion à la base de données
         $bd = $this->connexion();
         $requete = "INSERT INTO voiture(numero_identifiant, numero_serie, modele, marque, couleur, date_achat) VALUES(:numero_identifiant, :numero_serie, :modele, :marque,  :couleur, :date_achat)";
         $reponse =$bd->prepare($requete);
@@ -37,7 +49,9 @@
         );
       }
 
+      //Fonction de suppression d'une voiture dans la base de données
       public function delete_voiture($id){
+        //Appel de fonction connexion se trouvant dans le fichier de Connexion à la base de données
         $bd = $this->connexion();
         $requete = "DELETE FROM voiture WHERE id = :id";
         $reponse = $bd->prepare($requete);
@@ -47,7 +61,9 @@
         );
       }
 
+      //Fonction de modification d'une voiture à partir de la base de données
       public function update_voiture(Voiture $voiture){
+        //Appel de fonction connexion se trouvant dans le fichier de Connexion à la base de données
         $bd = $this->connexion();
         $requete = "UPDATE voiture SET numero_identifiant=:numero_identifiant, numero_serie=:numero_serie, modele=:modele, marque=:marque, couleur=:couleur, date_achat=:date_achat WHERE id=:id";
         $reponse = $bd->prepare($requete);
